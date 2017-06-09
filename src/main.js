148eb00413bf13c8084d81ae33fb332c5db0a500
michael <4apaev@gmail.com>
2017-06-09 14:58:22 +0300

    edit sync

--- src/main.js
+++ src/main.js
@@ -1,72 +1,95 @@
-((exports, URL, icon) => {
-    const app = exports.app = { init, BORKS: [] }
+((URL, icon, App) => {
+    App.init = init
     function init(center) {
-        app.map = new google.maps.Map($('map'), {
+        App.map = new google.maps.Map($('map'), {
             center,
             zoom: 16
           })
-        app.marker = new google.maps.Marker({
-            map: app.map,
+        App.marker = new google.maps.Marker({
+            map: App.map,
             position: center,
             draggable: true
           })
-        app.marker.addListener('click', e => {
-            confirm('add inspector?') && sync('add')
+        App.marker.addListener('click', () => {
+            confirm('add inspector?') && save()
           })
-        app.marker.addListener('dragend', e => {
+        App.marker.addListener('dragend', () => {
             sync()
           })
         sync()
       }
-    function sync(...args) {
-        return fetch(`${ [ URL, ...args ].join('/') }?lat=${ app.marker.position.lat() }&lng=${ app.marker.position.lng() }`)
+
+    function sync() {
+        let url = `${ URL }?lat=${ App.marker.position.lat() }&lng=${ App.marker.position.lng() }`
+        for(let k in App.params)
+          url += `&${ k }=${ App.params[ k ] }`
+        return fetch(url)
                 .then(x => x.json())
                 .then(resolve)
                 .catch(fail)
       }
+
+    function save() {
+        let body = App.marker.position.toJSON()
+        return fetch(URL, {
+                  method: 'POST',
+                  body: JSON.stringify(body)
+                })
+                .then(x => x.json())
+                .then(resolve)
+                .catch(fail)
+      }
+
     function resolve({ err, result }) {
         if (Array.isArray(result)) {
-          clear(), result.forEach(spawn)
+          clear();
+          result.forEach(spawn)
         } else if (result) {
           spawn(result)
         } else {
           fail(err)
         }
       }
+
     function spawn({ loc, updated }) {
-        app.BORKS.push(new google.maps.Marker({
+        App.BORKS.push(new google.maps.Marker({
           icon,
           updated,
-          map: app.map,
-          position: {
-            lat: loc[ 0 ],
-            lng: loc[ 1 ]
-          }
+          map: App.map,
+          position: { lat: loc[ 0 ],lng: loc[ 1 ] }
         }))
       }
+
     function clear() {
-        app.BORKS.forEach(bork => bork.setMap(null))
-        app.BORKS.length = 0
+        App.BORKS.forEach(bork => bork.setMap(null))
+        App.BORKS.length = 0
       }
+
     function fail(err) {
         $('message').textContent = err.message
         $('stack').textContent = err.stack
         $('modal').removeAttribute('style')
       }
-  })(this, `https://localhost:3000/v2/borks`, {
+
+  })(`https://localhost:3000/v2/borks`, {
     scale: .5,
     fillColor: 'tomato',
     strokeColor: 'crimson',
     fillOpacity: 1,
     strokeWeight: 2,
     path: `M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z`
-  })
+  }, this.App||(this.App={
+    BORKS: [],
+    params: { d:30 },
+    Pos: {
+      geo(x) { return { lat: x.coords.latitude, lng: x.coords.longitude }},
+      evt(x) { return x.latLng.toJSON() },
+      pos(x) { return x.position.toJSON() },
+      center(x) { return x.center.toJSON() },
+    }}));
 
 document.addEventListener('DOMContentLoaded', () => {
-    navigator.geolocation.getCurrentPosition(pos => app.init({
-      lat: pos.coords.latitude,
-      lng: pos.coords.longitude
-    }), err => app.init({
+    navigator.geolocation.getCurrentPosition(pos => App.init(App.Pos.geo(pos)), err => App.init({
       lat: 32.097,
       lng: 34.790
     }), {
